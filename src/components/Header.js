93857844fe777bca42e80a0aef694a0d8b046eb9
YuvRaj6392 <74025229+YuvRaj6392@users.noreply.github.com>
2022-10-07 00:00:39 +0530

    added logout functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,43 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Link,useNavigate } from 'react-router-dom'
 export default function Header() {
+  useEffect(()=>{
+    if(!localStorage.getItem('token'))
+    {
+      document.getElementById('logout').style.display='none';
+      document.getElementById('login').style.display='block';
+      document.getElementById('register').style.display='block';
+
+    }
+    else
+    {
+      document.getElementById('logout').style.display='block';
+      document.getElementById('login').style.display='none';
+      document.getElementById('register').style.display='none';
+    }
+
+  },[localStorage.getItem('token')])
+  const history=useNavigate();
+  const logoutHandler=(e)=>{
+    e.preventDefault();
+    
+     const id=localStorage.getItem('id');
+     const data={
+      id:id
+     }
+     fetch("http://localhost:8080/api/logout",{
+      method:"POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body:JSON.stringify(data)
+    }).then(res=>res.json()).then(data=>{
+      console.log(data);
+      localStorage.clear();
+      history('/')
+    })
+
+  }
   return (
     <div>
     
@@ -12,7 +49,7 @@ export default function Header() {
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
+        {/* <li className="nav-item">
           <Link className="nav-link" aria-current="page" to="/">Home</Link>
         </li>
         <li className="nav-item">
@@ -22,13 +59,15 @@ export default function Header() {
         <li className="nav-item">
           <Link className="nav-link" href="/">Contact Us</Link>
         </li>
-       
+        */}
        
       </ul>
       <form className="d-flex">
         <Link className="btn btn-success"  id='login' to='/login' >Login</Link>
 
         <Link className=" ms-2 btn btn-success"  id='register' to='/register'>Register</Link>
+
+        <Link className=" ms-2 btn btn-success"  id='logout' onClick={logoutHandler} >Logout</Link>
 
       </form>
   
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,13 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { Link,useNavigate } from 'react-router-dom'
 export default function Home() {
+  const history=useNavigate();
+  useEffect(()=>{
+    if(!localStorage.getItem('token'))
+    {
+      history('/')
+    }
+  },)
   return (
     <div>welcome to the home page</div>
   )
