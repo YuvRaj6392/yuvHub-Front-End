c08c484a0d6b2d02347659dc0e1870418de25c90
YuvRaj6392 <74025229+YuvRaj6392@users.noreply.github.com>
2022-10-08 01:08:13 +0530

    added changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,50 @@
-import React, { useEffect } from 'react'
-import { Link,useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 export default function Home() {
+  const [data1,setData1]=useState("")
   const history=useNavigate();
+  const token=localStorage.getItem('token');
+  
+  
+  
   useEffect(()=>{
+    
+    
     if(!localStorage.getItem('token'))
     {
       history('/')
     }
-  },)
+    else
+    {
+      fetch("http://localhost:8080/api/tutorial/show",{
+        method:"GET",
+        headers: {
+          'Content-Type': 'application/json',
+          'x-access-token':token
+        },
+        
+      }).then(res=>res.json()).then(data=>{
+     
+      setData1(data)
+      //  console.log(data1)
+        
+      })
+  
+}
+   
+  },[data1])
+
+  
   return (
-    <div>welcome to the home page</div>
+    <div style={{display:'flex'}}>
+    
+      {
+        data1 && data1.map(ele=>{
+          return <div key={ele._id}>
+            <span>{ele.category}</span>
+          </div>
+        })
+      }
+    </div>
   )
 }
